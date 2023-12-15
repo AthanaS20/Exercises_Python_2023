095b909b28ad6ceee39232acfef1a08792e286b6
ATHANA SILVA <athanasilva95@gmail.com>
2023-12-14 22:08:44 -0300

    exercicio

---
+++ Exercises/reverse_list.py
@@ -0,0 +1,17 @@
+l1 = [2,4,3]
+l2 = [5,6,4]
+
+
+l1.reverse()
+l2.reverse()
+
+for i in range(0, len(l1)):
+    somar =+ 1
+for j in range(0, len(l2)):
+    somar += 1
+    print(i + j)
+       
+        
+   
+    
+
