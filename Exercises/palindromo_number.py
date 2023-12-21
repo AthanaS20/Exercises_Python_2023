5216519d8c147b6735b8d21ab1d10354fcb6e142
ATHANA SILVA <athanasilva95@gmail.com>
2023-12-21 20:30:44 -0300

    update

--- Exercises/palindromo_number.py
+++ Exercises/palindromo_number.py
@@ -1,7 +1,23 @@
-numero = '111'
+def palindomer(n: int):
+    if (n < 0) or (n > 0 and n % 10 == 0):
+        return False
+    
+    half = 0
+    while n > half:
+        half = (half * 10) + n % 10 # 12
+        n = n // 10 # 12
+        if n == half or n == half // 10:
+            return True
+        
+        
+
+print(palindomer(121))
+
+     
 
 
-if numero == numero[::-1]:
-    print(True)
-else:
-    print(False)
+
+
+
+
+
