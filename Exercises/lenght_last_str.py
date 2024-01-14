44d7b476eab5ca61cff8569057156e3d4e0f99fe
ATHANA SILVA <athanasilva95@gmail.com>
2024-01-14 15:18:04 -0300

    solved

--- Exercises/lenght_last_str.py
+++ Exercises/lenght_last_str.py
@@ -1,11 +1,25 @@
 
 
 
-s = "Hello World"
-# s = ''.join(s.split())
+s = "   fly me   to   the moon "
+size = len(s)
+size_last_w = 0
 
-print(len(s))
 
+
+
+for i in range(size-1, -1, -1):
+    if s[i] != " ":
+        size_last_w += 1
+    
+    elif size_last_w > 0:
+        break
+
+print(size_last_w)
+        
+        
+    
+    
 
 
    
