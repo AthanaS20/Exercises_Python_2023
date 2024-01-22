710452f5c30e28bd4f848edd040635f5a61a9a69
ATHANA SILVA <athanasilva95@gmail.com>
2024-01-22 18:40:19 -0300

    update

--- plus_one.py
+++ plus_one.py
@@ -1,16 +1,22 @@
 digits = [9]
 
 
-for i in range(len(digits)):
+for i in range(len(digits) -1, -1, -1):
     
     
-    if digits[-1]:
-       digits[-1] = digits[-1] + 1
-    if digits[i] <= 9:
-        digits.
-   
-    print(digits)
-    break
+    if digits[i] == 9:
+        digits[i] = 0
+    
+    else:
+        digits[i] += 1
+        break
+
+if digits[0]:
+    digits.insert(0,1)
+    
+        
+    
+
 
         
     
