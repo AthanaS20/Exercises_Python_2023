0f8aa99f67ec296f7d4a6dfa3c9c7d3b39ffd73d
ATHANA SILVA <athanasilva95@gmail.com>
2024-01-25 20:38:06 -0300

    sqrt

---
+++ sqrt.py
@@ -0,0 +1,24 @@
+
+
+
+def sqrt(x):
+    count = 0
+    for i in range(1, x + 1):
+        if i * i == x:
+            count += 1
+            return i
+        
+        
+        
+    
+        
+        
+       
+    
+       
+      
+
+
+
+x = 6
+print(sqrt(x))
