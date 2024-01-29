33db8473c3c4db41c090f4dd722af824858bbe29
ATHANA SILVA <athanasilva95@gmail.com>
2024-01-29 20:51:20 -0300

    Update sqrt.py

--- sqrt.py
+++ sqrt.py
@@ -1,24 +1,36 @@
 
+# minha solução, só passou em um teste
 
+# def sqrt(x):
+#     count = 0
+#     for i in range(1, x + 1):
+#         if i * i == x:
+#             count += 1
+#             return i
 
-def sqrt(x):
-    count = 0
-    for i in range(1, x + 1):
-        if i * i == x:
-            count += 1
-            return i
-        
-        
-        
+# x = 6
+# print(sqrt(x))
+
+# solução correta
+
+def mysqrt(x):
     
-        
-        
-       
-    
-       
-      
+    left = 0
+    right = x 
 
-
+    while left <= right:
+        mid = (left + right) // 2  # mid 3
+       
+        if mid * mid < x:
+            left = mid +1 # 4
+         
+        elif mid * mid > x:
+            right = mid -1 # 3
+           
+        else:
+            return mid
+        
+    return right
 
 x = 6
-print(sqrt(x))
+print(mysqrt(x))
