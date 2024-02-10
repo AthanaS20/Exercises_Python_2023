a59b355c0fa1c55d29fe27f787d05beee28fed2f
ATHANA SILVA <athanasilva95@gmail.com>
2024-02-09 23:26:35 -0300

    tentativa de resolver exercicio

---
+++ Exercises/remove_duplicate.py
@@ -0,0 +1,27 @@
+head = [1,1,2,3,3]
+sorted_head = sorted(head)
+
+store_duplicates = []
+set_elements = set()
+
+for i in range(len(sorted_head)):
+    
+    for j in range(1,len(sorted_head)):
+        if sorted_head[0] == sorted_head:
+            store_duplicates.append(i)
+            print(sorted_head)
+
+
+            
+    
+        
+    
+        
+    
+    
+
+
+
+    
+    
+    
